5746430866a4853e80982e0fd4f76800e57f47fc
Josiel Oliveira <josieloliveira@hotmail.com>
2017-05-31 10:55:29 -0300

    Merged beingClass into being service

--- doc/beingClass_install.sql
+++
@@ -1,8 +0,0 @@
-create role mudengine_being_class_app login;
-alter role mudengine_being_class_app with password 'mudengine_being_class_app';
-
-create role mudengine_being_class CREATEDB;
-create schema mudengine_being_class authorization mudengine_being_class;
-grant mudengine_being_class to mudengine_being_class_app;
-
-alter role mudengine_being_class_app set search_path to mudengine_being_class;
