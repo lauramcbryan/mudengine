5866dcce80f292f8c60884d9403387059a139ee9
Josiel Oliveira <josieloliveira@hotmail.com>
2018-01-10 15:24:24 -0200

    Externalizing configurations.

--- mud-player/src/main/java/com/jpinfo/mudengine/player/util/PlayerSecurityAdapter.java
+++ mud-player/src/main/java/com/jpinfo/mudengine/player/util/PlayerSecurityAdapter.java
@@ -1,11 +1,28 @@
 package com.jpinfo.mudengine.player.util;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import com.jpinfo.mudengine.common.security.CommonSecurityAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.jpinfo.mudengine.common.security.CommonSecurityFilter;
+
+@Profile("!default")
 @Configuration
 @EnableWebSecurity
-public class PlayerSecurityAdapter extends CommonSecurityAdapter {
+public class PlayerSecurityAdapter extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/player/*").authenticated();
+		//http.authorizeRequests().antMatchers(HttpMethod.PUT, "/player/{\\x+}/session").permitAll();			
+		//http.authorizeRequests().antMatchers(HttpMethod.POST, "/player/{\\x+}/password").permitAll();
+		//http.authorizeRequests().antMatchers(HttpMethod.GET, "/health").permitAll();
+		//http.authorizeRequests().antMatchers(HttpMethod.PUT, "/player/{\\x+}").permitAll();		
+		http.addFilterBefore(new CommonSecurityFilter(), UsernamePasswordAuthenticationFilter.class);
+	}
 }
