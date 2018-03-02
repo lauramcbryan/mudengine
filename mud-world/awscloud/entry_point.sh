a4fe2449392a6c5cd812495abc516548a7dcf140
josieloliveira <josieloliveira@hotmail.com>
2018-03-01 22:07:25 -0300

    Including check to verify if it is in AWS environment

--- mud-world/awscloud/entry_point.sh
+++ mud-world/awscloud/entry_point.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -o errexit
 set -o xtrace
 
-export EC2_HOST=$(wget -O - http://169.254.169.254/latest/meta-data/local-ipv4 2> /dev/null)
-result="$(python /opt/bin/ecs-get-port-mapping.py)"
-eval "$result"
+if [[ "${DEPLOY_ENV:-default}" = "AWS" ]]; then
+	export EC2_HOST=$(wget -O - http://169.254.169.254/latest/meta-data/local-ipv4 2> /dev/null)
+	result="$(python ecs-get-port-mapping.py)"
+	eval "$result"
+fi
 
 java -jar -Djava.security.egd=file:/dev/./urandom app.jar
