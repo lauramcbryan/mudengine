ad6ca071fb2465d177d81c54640288f81e59f2c8
josieloliveira <josieloliveira@hotmail.com>
2018-03-04 00:06:09 -0300

    Updating eureka client library. Overriding default eureka client configuration.

--- mud-world/src/main/java/com/jpinfo/mudengine/world/MudWorldApplication.java
+++ mud-world/src/main/java/com/jpinfo/mudengine/world/MudWorldApplication.java
@@ -3,8 +3,11 @@ package com.jpinfo.mudengine.world;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,4 +18,12 @@ public class MudWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MudWorldApplication.class, args);
 	}
+	
+	
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+		  EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+		  
+		  return b;
+	}	
 }
