7a27e974dc6f645a00f57cac310f029634fc0143
JosielOliveira <josieloliveira@ext.cvc.com.br>
2018-04-02 16:53:17 -0300

    Upgrading spring version to 2.0.0

--- mud-world/src/main/java/com/jpinfo/mudengine/world/client/ItemServiceClient.java
+++ mud-world/src/main/java/com/jpinfo/mudengine/world/client/ItemServiceClient.java
@@ -1,6 +1,7 @@
 package com.jpinfo.mudengine.world.client;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+
+import org.springframework.cloud.openfeign.FeignClient;
 
 import com.jpinfo.mudengine.common.service.ItemService;
 
