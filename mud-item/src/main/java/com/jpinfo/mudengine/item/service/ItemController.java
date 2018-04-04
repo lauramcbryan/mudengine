416e03f8e8c018e1d8c6e35c5cedea3baf99490b
JosielOliveira <josieloliveira@ext.cvc.com.br>
2018-04-04 19:47:54 -0300

    Upgrading to Spring 2.0 release

--- mud-item/src/main/java/com/jpinfo/mudengine/item/service/ItemController.java
+++ mud-item/src/main/java/com/jpinfo/mudengine/item/service/ItemController.java
@@ -1,6 +1,7 @@
 package com.jpinfo.mudengine.item.service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -37,13 +38,11 @@ public class ItemController implements ItemService {
 		
 		Item response = null;
 		
-		MudItem dbItem = itemRepository.findOne(itemId);
+		MudItem dbItem = itemRepository
+				.findById(itemId)
+				.orElseThrow(() -> new EntityNotFoundException("Item entity not found"));
 		
-		if (dbItem!=null) {
-			response = ItemHelper.buildItem(dbItem);
-		} else {
-			throw new EntityNotFoundException("Item entity not found"); 
-		}
+		response = ItemHelper.buildItem(dbItem);
 		
 		return response;
 	}
@@ -54,66 +53,62 @@ public class ItemController implements ItemService {
 		Item response = null;
 		
 		// Retrieving the database record
-		MudItem dbItem = itemRepository.findOne(itemId);
+		MudItem dbItem = itemRepository.findById(itemId)
+				.orElseThrow(() -> new EntityNotFoundException("Item entity not found"));
 		
-		if (dbItem!=null) {
+		dbItem.setCurWorld(requestItem.getCurWorld());
+		dbItem.setCurPlaceCode(requestItem.getCurPlaceCode());
+		dbItem.setQuantity(requestItem.getQuantity());
+		dbItem.setCurOwner(requestItem.getCurOwner());
 		
-			dbItem.setCurWorld(requestItem.getCurWorld());
-			dbItem.setCurPlaceCode(requestItem.getCurPlaceCode());
-			dbItem.setQuantity(requestItem.getQuantity());
-			dbItem.setCurOwner(requestItem.getCurOwner());
+		// Adjusting the attributes in database against those in request
+		// (This is done even if the itemClass has changed the attributes)
 			
-			// Looking for attributes to remove
+		// Looking for attributes to remove
+		List<MudItemAttr> removeList = 
+			dbItem.getAttrs().stream()
+				// Filtering attributes from db record that doesn't exist in request
+				.filter(d -> !requestItem.getAttrs().containsKey(d.getId().getAttrCode()))
+				.collect(Collectors.toList());
+
+		// Remove from list all records found
+		dbItem.getAttrs().removeAll(removeList);
+
+		// Looking for attributes to add
+		for(String curAttr: requestItem.getAttrs().keySet()) {
+			
+			boolean found = false;
 			for(MudItemAttr curItemAttr: dbItem.getAttrs()) {
 				
-				// If it not exists in request, remove it
-				if (requestItem.getAttrs().get(curItemAttr.getId().getAttrCode())==null) {
-					dbItem.getAttrs().remove(curItemAttr);
+				if (curItemAttr.getId().getAttrCode().equals(curAttr)) {
+					curItemAttr.setAttrValue(requestItem.getAttrs().get(curAttr));
+					found = true;
 				}
 			}
 			
-			// Looking for attributes to add
-			for(String curAttr: requestItem.getAttrs().keySet()) {
-				
-				boolean found = false;
-				for(MudItemAttr curItemAttr: dbItem.getAttrs()) {
-					
-					if (curItemAttr.getId().getAttrCode().equals(curAttr)) {
-						curItemAttr.setAttrValue(requestItem.getAttrs().get(curAttr));
-						found = true;
-					}
-				}
-				
-				if (!found) {
-					dbItem.getAttrs().add(
-							ItemHelper.buildMudItemAttr(dbItem.getItemCode(), curAttr, requestItem.getAttrs().get(curAttr))
-							);
-				}
+			if (!found) {
+				dbItem.getAttrs().add(
+						ItemHelper.buildMudItemAttr(dbItem.getItemCode(), curAttr, requestItem.getAttrs().get(curAttr))
+						);
 			}
-			
-		
-			// if the itemClass is changing, reset the attributes
-			if (!dbItem.getItemClass().getItemClass().equals(requestItem.getItemClassCode())) {
-			
-				MudItemClass dbClassItem = itemClassRepository.findOne(requestItem.getItemClassCode());
-				
-				if (dbClassItem!=null) {
-					dbItem = ItemHelper.changeItemAttrs(dbItem, dbItem.getItemClass(), dbClassItem);
-					
-					dbItem.setItemClass(dbClassItem);
-					
-				} else {
-					throw new EntityNotFoundException("Item Class entity not found");
-				}
-			}
-			
-			dbItem = itemRepository.save(dbItem);
-			
-			response = ItemHelper.buildItem(dbItem);
-			
-		} else {
-			throw new EntityNotFoundException("Item entity not found");
 		}
+
+		// if the itemClass is changing, reset the attributes
+		if (!dbItem.getItemClass().getItemClass().equals(requestItem.getItemClassCode())) {
+		
+			MudItemClass dbClassItem = itemClassRepository
+					.findById(requestItem.getItemClassCode())
+					.orElseThrow(() -> new EntityNotFoundException("Item Class entity not found"));
+
+			// Replace all current attributes from old class by the new one
+			dbItem = ItemHelper.changeItemAttrs(dbItem, dbItem.getItemClass(), dbClassItem);
+			dbItem.setItemClass(dbClassItem);
+		} 
+		
+		
+		dbItem = itemRepository.save(dbItem);
+		
+		response = ItemHelper.buildItem(dbItem);
 		
 		return response;
 	}
@@ -126,41 +121,39 @@ public class ItemController implements ItemService {
 		// Check if a minimum parameters are present
 		if (owner.isPresent() || (placeCode.isPresent() && worldName.isPresent())) {
 		
-			MudItemClass dbClassItem = itemClassRepository.findOne(itemClassCode);
+			MudItemClass dbClassItem = itemClassRepository
+					.findById(itemClassCode)
+					.orElseThrow(() -> new EntityNotFoundException("Item Class entity not found"));
 			
-			if (dbClassItem!=null) {
-				
-				MudItem newDbItem = new MudItem();
-				newDbItem.setItemClass(dbClassItem);
-				
-				if (worldName.isPresent())
-					newDbItem.setCurWorld(worldName.get());
-				
-				if (placeCode.isPresent())
-					newDbItem.setCurPlaceCode(placeCode.get());
-				
-				if (owner.isPresent())
-					newDbItem.setCurOwner(owner.get());
+			MudItem newDbItem = new MudItem();
+			newDbItem.setItemClass(dbClassItem);
+			
+			if (worldName.isPresent())
+				newDbItem.setCurWorld(worldName.get());
+			
+			if (placeCode.isPresent())
+				newDbItem.setCurPlaceCode(placeCode.get());
+			
+			if (owner.isPresent())
+				newDbItem.setCurOwner(owner.get());
 
-				if (quantity.isPresent())
-					newDbItem.setQuantity(quantity.get());
-				else
-					newDbItem.setQuantity(1);
+			if (quantity.isPresent())
+				newDbItem.setQuantity(quantity.get());
+			else
+				newDbItem.setQuantity(1);
+		
+			// Saving the entity (to get the itemCode)
+			newDbItem = itemRepository.save(newDbItem);
+		
+			// Populating the attrs
+			newDbItem = ItemHelper.changeItemAttrs(newDbItem, null, dbClassItem);
+		
+			// Saving again
+			itemRepository.save(newDbItem);
+		
+			// Building the response
+			response = ItemHelper.buildItem(newDbItem);
 			
-				// Saving the entity (to get the itemCode)
-				newDbItem = itemRepository.save(newDbItem);
-			
-				// Populating the attrs
-				newDbItem = ItemHelper.changeItemAttrs(newDbItem, null, dbClassItem);
-			
-				// Saving again
-				itemRepository.save(newDbItem);
-			
-				// Building the response
-				response = ItemHelper.buildItem(newDbItem);
-			} else {
-				throw new EntityNotFoundException("Item Class entity not found");
-			}
 		} else {
 			throw new IllegalParameterException("At least owner or place must be set in request");
 		}
@@ -172,15 +165,11 @@ public class ItemController implements ItemService {
 	public void destroyItem(@RequestHeader(TokenService.HEADER_TOKEN) String authToken, @PathVariable Long itemId) {
 
 		// Retrieving the database record
-		MudItem dbItem = itemRepository.findOne(itemId);
+		MudItem dbItem = itemRepository
+				.findById(itemId)
+				.orElseThrow(() -> new EntityNotFoundException("Item entity not found"));
 		
-		if (dbItem!=null) {
-			
-			itemRepository.delete(dbItem);
-			
-		} else {
-			throw new EntityNotFoundException("Item entity not found"); 
-		}
+		itemRepository.delete(dbItem);
 	}
 
 	@Override
@@ -190,10 +179,10 @@ public class ItemController implements ItemService {
 		
 		List<MudItem> dbResponse = itemRepository.findByCurWorldAndCurPlaceCode(worldName, placeCode);
 		
-		for(MudItem curdbItem: dbResponse) {
-			
-			responseList.add(ItemHelper.buildItem(curdbItem));
-		}
+		
+		dbResponse.stream().forEach(d-> {
+			responseList.add(ItemHelper.buildItem(d));
+		});
 		
 		return responseList;
 	}
@@ -205,10 +194,9 @@ public class ItemController implements ItemService {
 		
 		List<MudItem> dbResponse = itemRepository.findByCurOwner(owner);
 		
-		for(MudItem curdbItem: dbResponse) {
-			
-			responseList.add(ItemHelper.buildItem(curdbItem));
-		}
+		dbResponse.stream().forEach(d-> {
+			responseList.add(ItemHelper.buildItem(d));
+		});
 		
 		return responseList;
 	}
@@ -218,27 +206,24 @@ public class ItemController implements ItemService {
 		
 		List<MudItem> dbResponse = itemRepository.findByCurWorldAndCurPlaceCode(worldName, placeCode);
 		
-		for(MudItem curdbItem: dbResponse) {
-			
-			itemRepository.delete(curdbItem);
-		}
-		
+		dbResponse.stream().forEach(d-> {
+			itemRepository.delete(d);
+		});
 	}
 
 	@Override
 	public void dropAllFromBeing(@RequestHeader(TokenService.HEADER_TOKEN) String authToken, @PathVariable Long owner, @RequestParam String worldName, @RequestParam Integer placeCode) {
 		
 		List<MudItem> dbResponse = itemRepository.findByCurOwner(owner);
-		
-		for(MudItem curdbItem: dbResponse) {
+
+		dbResponse.stream().forEach(d-> {
 			
-			curdbItem.setCurOwner(null);
-			curdbItem.setCurWorld(worldName);
-			curdbItem.setCurPlaceCode(placeCode);
+			d.setCurOwner(null);
+			d.setCurWorld(worldName);
+			d.setCurPlaceCode(placeCode);
 			
-			itemRepository.save(curdbItem);
-		}
-		
+			itemRepository.save(d);
+		});
 	}
 	
 }
