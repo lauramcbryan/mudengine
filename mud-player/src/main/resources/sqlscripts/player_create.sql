f8a631aa4eeed380a70b3c980338429b85a91925
josieloliveira <josieloliveira@hotmail.com>
2018-04-05 07:47:47 -0300

    Updating sql scripts.

--- mud-player/src/main/resources/sqlscripts/player_create.sql
+++ mud-player/src/main/resources/sqlscripts/player_create.sql
@@ -10,17 +10,32 @@ CREATE TABLE MUDENGINE_PLAYER.MUD_PLAYER (
 		PASSWORD		varchar(64) not null,
 		EMAIL			varchar(30) not null,
 		NAME			varchar(30),
-		LANGUAGE		varchar(30),
-		COUNTRY			varchar(5),
+		LOCALE  		varchar(30),
+		INSERT_DATE		timestamp not null default current_timestamp,		
 		STATUS			integer not null default 0,
 		CONSTRAINT MUD_PLAYER_PK PRIMARY KEY (PLAYER_ID)
 );	
+
+CREATE TABLE MUDENGINE_PLAYER.MUD_PLAYER_BEING (
+	PLAYER_ID			bigint not null,
+	BEING_CODE			bigint not null,	
+	BEING_NAME			varchar(50) not null,	
+	BEING_CLASS			varchar(30) not null,
+	LAST_PLAYED			timestamp,	
+	CONSTRAINT MUD_PLAYER_BEING_PK PRIMARY KEY (PLAYER_ID, BEING_CODE),
+	FOREIGN KEY (PLAYER_ID) REFERENCES MUDENGINE_PLAYER.MUD_PLAYER(PLAYER_ID) on delete cascade
+);
+
 
 CREATE TABLE MUDENGINE_PLAYER.MUD_PLAYER_SESSION (
 	SESSION_ID			bigint not null,
 	PLAYER_ID			bigint not null,
 	SESSION_START		timestamp not null default current_timestamp,
 	SESSION_END			timestamp,
+	CLIENT_TYPE			varchar(10),
+	IP_ADDRESS			varchar(16),
+	BEING_CODE			bigint not null,
+	
 	CONSTRAINT MUD_PLAYER_SESSION_PK PRIMARY KEY (SESSION_ID),
 	FOREIGN KEY (PLAYER_ID) REFERENCES MUDENGINE_PLAYER.MUD_PLAYER(PLAYER_ID) on delete cascade
 );
