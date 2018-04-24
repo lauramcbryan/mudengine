b8f5a3734ec3f1c8cf3f4fc17519c78a92e9b50c
josieloliveira <josieloliveira@hotmail.com>
2018-04-23 23:47:48 -0300

    Creating project for reverse proxy

---
+++ mud-api/src/test/java/com/jpinfo/mudengine/mudapi/MudApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jpinfo.mudengine.mudapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MudApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
