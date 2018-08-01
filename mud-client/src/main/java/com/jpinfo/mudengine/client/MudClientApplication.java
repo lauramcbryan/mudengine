fd7623077730faae296350e1cb17681475773d8d
JosielOliveira <josieloliveira@ext.cvc.com.br>
2018-08-01 09:58:29 -0300

    Changing the Transformer to strip control characters

--- mud-client/src/main/java/com/jpinfo/mudengine/client/MudClientApplication.java
+++ mud-client/src/main/java/com/jpinfo/mudengine/client/MudClientApplication.java
@@ -9,7 +9,6 @@ import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.ip.tcp.TcpInboundGateway;
 import org.springframework.integration.ip.tcp.connection.TcpNetServerConnectionFactory;
 import org.springframework.integration.ip.tcp.serializer.ByteArraySingleTerminatorSerializer;
-import org.springframework.integration.transformer.ObjectToStringTransformer;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -46,7 +45,7 @@ public class MudClientApplication {
 	@Bean
 	@Transformer(inputChannel = "requestChannel", outputChannel="plainRequestChannel")
 	public org.springframework.integration.transformer.Transformer getRequestTransformer() {
-		return new ObjectToStringTransformer();
+		return new TelnetTransformer();
 	}
 	
 	
---
+++ mud-client/src/main/java/com/jpinfo/mudengine/client/TelnetTransformer.java
@@ -0,0 +1,30 @@
+package com.jpinfo.mudengine.client;
+
+import java.util.Arrays;
+
+import org.springframework.integration.transformer.ObjectToStringTransformer;
+
+public class TelnetTransformer extends ObjectToStringTransformer {
+
+	@Override
+	protected String transformPayload(Object payload) throws Exception {
+		
+		if (payload instanceof byte[]) {
+			
+			byte[] byteArrayPayload = (byte[])payload;
+			
+			// Check if there's control characters at start of payload
+			int k=0;
+			for(;(k<byteArrayPayload.length) && (byteArrayPayload[k] < 32);k++);
+			
+			// Call super method passing just the remaining buffer
+			return super.transformPayload(
+					Arrays.copyOfRange(byteArrayPayload, k, byteArrayPayload.length)
+					);
+			
+		}
+		
+		return super.transformPayload(payload);
+	}
+
+}
