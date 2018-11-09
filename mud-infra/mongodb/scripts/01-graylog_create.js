945c6ad006dad4cee5601d41a63c35003ef7c77e
Josiel Oliveira <josieloliveira@ext.cvc.com.br>
2018-11-09 19:31:20 -0200

    Moving scripts to mud-infra repository (not the project)

--- mud-infra/mongodb/scripts/01-graylog_create.js
+++
@@ -1,9 +0,0 @@
-var db = connect("127.0.0.1:27017/graylog");
-
-db.createUser(
-	{
-		user: "mudlog",
-		pwd: "mudlog",
-		roles: [ "dbOwner" ]
-	}
-);
--- mud-infra/postgres/docker/scripts/01-action_install.sql
+++
@@ -1,8 +0,0 @@
-create role mudengine_action_app login;
-alter role mudengine_action_app with password 'mudengine_action_app';
-
-create role mudengine_action CREATEDB;
-create schema mudengine_action authorization mudengine_action;
-grant mudengine_action to mudengine_action_app;
-
-alter role mudengine_action_app set search_path to mudengine_action;
