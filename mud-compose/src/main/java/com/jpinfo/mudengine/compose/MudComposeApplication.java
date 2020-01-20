a04310b66dcfc703623601f9c3db9f2b3f27c1e2
Josiel Oliveira <josieloliveira@hotmail.com>
2020-01-20 18:15:29 -0300

    Mud-compose project created.  INTERIM COMMIT.

---
+++ mud-compose/src/main/java/com/jpinfo/mudengine/compose/MudComposeApplication.java
@@ -0,0 +1,13 @@
+package com.jpinfo.mudengine.compose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudComposeApplication.class, args);
+	}
+
+}
---
+++ mud-compose/src/test/java/com/jpinfo/mudengine/compose/MudComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpinfo.mudengine.compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
